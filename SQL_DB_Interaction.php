d1cdf677ea29068e81ec9041a6ab3aa3bd1c3053
Leo <leo.moncoiffet@etud.univ-angers.fr>
2024-01-31 11:28:44 +0100

    First Commit

---
+++ SQL_DB_Interaction.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>
+            Connecting to MySQL DB
+        </title>
+        <body>
+            <?php
+            $servername = "localhost";  // Replace with your database host
+            $username = "root";      // Replace with your database username
+            $password = "";      // Replace with your database password
+            $dbname = "badui";            // Replace with your database name
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            echo "Connected successfully";
+            ?>
+        </body>
+    </head>
+</html>
