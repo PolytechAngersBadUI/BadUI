f08bcecaaaaaa2336e4ed45924cb1f4fc353f2f7
Leo Moncoiffet <leo.moncoiffet@etud.univ-angers.fr>
2024-02-07 14:40:49 +0100

    Access all values from the table aswell and echo them in processnumber php(mainly for testing)

--- process_number.php
+++ process_number.php
@@ -1,28 +1,38 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  include 'connect_database.php'; // Assuming this file contains the database connection code
+  include 'connect_database.php'; // Contains connection to database
 
-  // Access the value sent from the JavaScript request
+  // We get the value from the js script
   $number = $_POST['number'];
   
-  // Echo the received number value (for debugging purposes)
+  // Echo value received
   echo "Received number value: " . $number;
 
-  // Prepare the SQL statement with a placeholder for the value
+  // Prepare the SQL statement with a placeholder instead of value
   $sql = "INSERT INTO workpls (number, comment) VALUES (?, 'Here is a comment I want to see')";
-  
-  // Prepare sql statement
   $stmt = $conn->prepare($sql);
 
   // Bind param
-  $stmt->bind_param("i", $number); // Assuming 'number' is an integer. Use "s" for string, "d" for double, etc.
+  $stmt->bind_param("i", $number);
 
   // Execute statement and check if error
   if ($stmt->execute()) {
     echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  //Test get all data from said table and echo it
+  $sql2 = "SELECT * FROM workpls";
+  $result = $conn->query($sql2);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Number: " . $row["number"]. " - Comment: " . $row["comment"]. "<br>";
+    }
+  } else {
+    echo "0 results";
   }
   //Close statement & database conn
   $stmt->close();
