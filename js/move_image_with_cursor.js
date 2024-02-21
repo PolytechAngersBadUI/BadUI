7a6e7a3dd7ce25468105c7148a71882db8d022ba
Léo Moncoiffet <127783517+Helfhris@users.noreply.github.com>
2024-02-21 10:05:16 +0100

    test cursor image start

---
+++ js/move_image_with_cursor.js
@@ -0,0 +1,21 @@
+// Get the image element
+const image = document.querySelector('img');
+
+// Add event listener to track mouse movement
+image.addEventListener('mousemove', moveImageWithCursor);
+
+// Function to move the image with the cursor
+function moveImageWithCursor(event) {
+  // Calculate the new position of the image based on the mouse coordinates
+  const x = event.clientX - image.width / 2;
+  const y = event.clientY - image.height / 2;
+
+  // Set the new position of the image
+  image.style.left = x + 'px';
+  image.style.top = y + 'px';
+}
+
+// Set a timeout to stop the image from following the cursor after 30 seconds
+setTimeout(() => {
+  image.removeEventListener('mousemove', moveImageWithCursor);
+}, 30000);
