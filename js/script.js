4fb8c49dedbf0ff3a2a26a2759414ef62abf409f
Léo Moncoiffet <127783517+Helfhris@users.noreply.github.com>
2024-02-21 11:03:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-let numbershown = 0;
-
-
-
 function increaseNumber() {
   const formData = new FormData();
   formData.append('operation', 1);
@@ -10,7 +6,7 @@ function increaseNumber() {
     body:formData
   })
     .then(response => response.text())
-    .then(data => console.log(data))
+    .then(data => document.getElementById("numberField").innerHTML = data)
     .catch(error => console.error('Error:', error));
 }
 
@@ -22,16 +18,14 @@ function decreaseNumber() {
     body:formData
   })
     .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    .then(data => document.getElementById("numberField").innerHTML = data)
+    .catch(error => console.error('Error:', error))
 }
 
 function sendToServer() {
-  let number = numbershown;
-  console.log(number);
   const formData = new FormData();
-  formData.append('number', number);
-  fetch('php/process_number.php', {
+  formData.append('operation', 3);
+  fetch('php/incdec.php', {
     method: 'POST',
     body: formData
   })
@@ -39,3 +33,4 @@ function sendToServer() {
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
 }
+
